4ac439357de1ad13c05af1d5d72f54ee6b4d0c89
FabianAAren <gusfabre@student.gu.se>
2017-11-28 17:59:03 +0100

    Not quite working random_walk, but for large possition weights it seems to work, small weights there is an index error. Must be solved

--- Project_friendship.m
+++ Project_friendship.m
@@ -1,11 +1,11 @@
-clear, clc
+clear, clc, close
 N = 200;
-lattice_size = 1000;
+lattice_size = 200;
 walkers_pos = randi(lattice_size,N,2);
 movability = 0.8;%rand(N,1); %probability to move each time step
-T = 100;
-k_1 = 0.06;
-k_2 = 0.06;
+T = 1000;
+k_1 = 0.03;
+k_2 = 0.01;
 
 Friend_NetworkMatrix = zeros(N);
 
@@ -19,17 +19,44 @@ for t = 1:T
     
     dr = (2*(rand(sum((logical_vector)),2)<.5)-1) .*[c<0.5,c>0.5];
     dr_walk=dr - k_1*(walkers_pos(...
-        logical_vector,:)-[200,200]) -k_2*(walkers_pos(logical_vector,:)...
-        -[554,920]);
+        logical_vector,:)-[20,20]) -k_2*(walkers_pos(logical_vector,:)...
+        -[180,100]);
     
-    dr_walk = dr_walk
+    norm_factor = sqrt((dr_walk(:,1).^2+dr_walk(:,2).^2));
+    dr_walk_norm = dr_walk./norm_factor; %normalised position change
+    prob_dirr = dr_walk_norm.^2;
+    
+    C = rand(length(dr_walk_norm),1); %apparently a stochastic term is needed
+    A = abs(prob_dirr-C);
+    closest = min(A,[],2);
+    
+    index = A == closest;
+    
+
+    
+    dr_walk = sign(dr_walk_norm(index));
+    
+    
+    if length(dr_walk) ~= length(index)
+        
+        c = rand;
+        logic = A(:,1) == A(:,2);
+        A(logic,:) = A(logic,1)*[c<0.5,c>0.5];
+        
+        index = A == closest;
+   
+    dr_walk = sign(dr_walk_norm(index));
+    
+    end
+    
+    dr_walk = index.*dr_walk;
     
     walkers_pos(logical_vector,:) =...
         mod(walkers_pos(logical_vector,:) +dr_walk ,lattice_size);
     
     scatter(walkers_pos(:,1),walkers_pos(:,2),'filled')
-     axis([0 lattice_size 0 lattice_size])
-
- pause(0.1)
-   clf
+    axis([0 lattice_size 0 lattice_size])
+    
+    pause(0.01)
+    clf
 end
