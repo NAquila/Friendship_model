1a9822510162272586f183ab0dc125b90b7a8806
niklas <niklas.renstrom@gmail.com>
2017-12-11 14:20:54 +0100

    Fixed bug with moving to left corner -- calculating energy differences instead

--- project/pot.m
+++ project/pot.m
@@ -1,7 +1,10 @@
 function v = pot(x,y,s,v0,u0,std,std2,mn,N,C)
 
+% Wij = potential from j acting on i
 W = u0 * exp(- ((x - C(:,1)').^2 + (y - C(:,2)').^2) / (2 * std2 * std2));
 
-v = v0 * exp(- ((x - mn(1)).^2 + (y - mn(2)).^2) / (2 * std * std)) + sum(N.* W,2) + (x < -s/2 | y < -s/2 | x > s/2 | y > s/2) * realmax;
-
+v = sum(v0 * exp(- ((x - mn(1)).^2 + (y - mn(2)).^2) / (2 * std * std)),2) ...
+    + sum(N.* W,2) ...
+    + (x < -s/2 | y < -s/2 | x > s/2 | y > s/2) * realmax;
+% disp(v(1))
 end
