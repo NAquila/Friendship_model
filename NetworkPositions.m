ae7aa53eb7317668e80cbbf3dd03b2dbd171aad0
niklas <niklas.renstrom@gmail.com>
2017-12-12 10:36:21 +0100

    More cleaning

--- NetworkPositions.m
+++
@@ -1,8 +0,0 @@
-function [a_network_x,a_network_y] = NetworkPositions(Adj_matrix,agent_x, agent_y)
-
-A = sparse(triu(Adj_matrix));
-
-[List_1, List_2] = find(A);
-
-a_network_x = [agent_x(List_1);agent_x(List_2)];
-a_network_y = [agent_y(List_2);agent_y(List_1)];
--- project/dec.m
+++
@@ -1,6 +0,0 @@
-function g = dec(t,l,T0)
-
-g = exp(- l * (t - T0));
-
-end
-
